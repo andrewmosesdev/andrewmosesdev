65609fd063560f5d72fa8656002883982ec54e5f
Andrew Moses <andrewmosesdrive@gmail.com>
2022-10-29 16:12:58 -0700

    content; analytics

--- components/TitleSection.tsx
+++ components/TitleSection.tsx
@@ -1,11 +1,11 @@
 const TitleSection = (): JSX.Element => (
   <section className="text-center">
     <h1 className="text-6xl">
-      <span className="text-5xl block mb-10">
-        {"{"} AM {"}"}
-      </span>
-      <span className="text-3xl mb-10 block">
-        Software Engineer | Web Developer
+      <span className="text-5xl block mb-10">Andrew Moses</span>
+      <span className="text-3xl mb-10 flex flex-col">
+        <span className="mb-[8px]">Software Engineer</span>
+        <span className="mb-[8px]">Web Developer</span>
+        <span className="mb-[8px]">JavaScript Tutor</span>
       </span>
     </h1>
   </section>
