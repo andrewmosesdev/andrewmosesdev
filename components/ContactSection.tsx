6ca24888f3209eeb29ea32a9c6de5020f9166023
Andrew Moses <andrewmosesdrive@gmail.com>
2022-10-29 18:51:10 -0700

    button spacing

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -8,7 +8,7 @@ const contactLinks = [
       <a
         href="https://www.linkedin.com/in/andrewjmoses/"
         key="linkedin"
-        className="rounded-sm border-white border-2 p-1 mx-1 hover:border-indigo-500 hover:bg-indigo-500 duration-300"
+        className="rounded-sm border-white border-2 p-1 mx-[8px] hover:border-indigo-500 hover:bg-indigo-500 duration-300"
         aria-label="Visit my Linkedin page"
       >
         <SiLinkedin className="text-lg " />
@@ -20,7 +20,7 @@ const contactLinks = [
       <a
         href="https://github.com/andrewmosesdev"
         key="github"
-        className="rounded-sm border-white border-2 p-1 mx-1 hover:border-indigo-500 hover:bg-indigo-500 duration-300"
+        className="rounded-sm border-white border-2 p-1 mx-[8px] hover:border-indigo-500 hover:bg-indigo-500 duration-300"
         aria-label="Visit my GitHub page"
       >
         <SiGithub className="text-lg " />
@@ -32,7 +32,7 @@ const contactLinks = [
       <a
         href="mailto: andrewmosesdev@gmail.com"
         key="email"
-        className="rounded-sm border-white border-2 p-1 mx-1 hover:border-indigo-500 hover:bg-indigo-500 duration-300"
+        className="rounded-sm border-white border-2 p-1 mx-[8px] hover:border-indigo-500 hover:bg-indigo-500 duration-300"
         aria-label="Send me an email"
       >
         <AiOutlineMail className="text-lg " />
@@ -44,7 +44,7 @@ const contactLinks = [
       <a
         href="https://docs.google.com/document/d/1ATXNNyOWpP7TMpHcFFAZKqai5InChXYfKXqlFvj7yZ0/edit?usp=sharing"
         key="resume"
-        className="rounded-sm border-white border-2 p-1 mx-1 hover:border-indigo-500 hover:bg-indigo-500 duration-300"
+        className="rounded-sm border-white border-2 p-1 mx-[8px] hover:border-indigo-500 hover:bg-indigo-500 duration-300"
         aria-label="View my resume"
       >
         <HiOutlineDocumentText className="text-lg " />
@@ -56,7 +56,9 @@ const contactLinks = [
 const ContactSection = (): JSX.Element => (
   <section className="w-full flex flex-col justify-center items-center">
     <h3 className="text-xl">Contact</h3>
-    <div className="flex mt-2">{contactLinks.map((link) => link.element)}</div>
+    <div className="flex mt-[16px]">
+      {contactLinks.map((link) => link.element)}
+    </div>
   </section>
 );
 
