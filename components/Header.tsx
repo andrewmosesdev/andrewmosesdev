0b60880b1c714224ac112c15ad82c69bec9cb239
Andrew Moses <andrewmosesdrive@gmail.com>
2022-10-29 18:41:18 -0700

    head/meta

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,10 @@ import Head from "next/head";
 
 const Header = () => (
   <Head>
-    <title>andrewmoses.dev</title>
+    <title>AM - Software Engineer | Web Developer | JavaScript Tutor</title>
     <meta
       name="description"
-      content="Experienced software engineer and web developer focused on creating fast, scalable, and accessible applications with strong SEO."
+      content="Experienced software engineer, web developer, and JavaScript tutor focused on creating fast, scalable, and accessible applications with strong SEO."
     />
     <link rel="shortcut icon" href="/icon.png" />
     <link rel="canonical" href="https://www.andrewmoses.dev" />
@@ -19,9 +19,8 @@ const Header = () => (
     <meta property="og:image" content="/icon.png" />
     <meta
       property="og:description"
-      content="Experienced software engineer and web developer focused on creating fast, scalable, and accessible applications with strong SEO."
+      content="Experienced software engineer, web developer, and JavaScript tutor focused on creating fast, scalable, and accessible applications with strong SEO."
     />
-    
   </Head>
 );
 
