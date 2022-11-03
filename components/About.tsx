b1e38ec0c51bb57d35aaa2f37bfec58372754168
Andrew Moses <andrewmosesdrive@gmail.com>
2022-11-02 17:40:50 -0700

    re-add js tutor stuff

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,9 @@
 const About = (): JSX.Element => (
   <section>
     <h2 className="mb-10 whitespace-normal text-center">
-      Experienced software engineer and web developer located in Phoenix,
-      Arizona. I focus on creating performant, scalable, and accessible
-      applications with strong SEO.
+      Experienced software engineer, web developer, and JavaScript tutor located
+      in Phoenix, Arizona. I focus on creating performant, scalable, and
+      accessible applications with strong SEO.
     </h2>
   </section>
 );
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,21 @@ const Header = () => (
     <title>AM - Software Engineer | Web Developer</title>
     <meta
       name="description"
-      content="Experienced software engineer and web developer focused on creating fast, scalable, and accessible applications with strong SEO."
+      content="Experienced software engineer, web developer, and JavaScript tutor focused on creating fast, scalable, and accessible applications with strong SEO."
     />
     <link rel="shortcut icon" href="/icon.png" />
     <link rel="canonical" href="https://www.andrewmoses.dev" />
 
     <meta
       property="og:title"
-      content="Andrew Moses - Software Engineer | Web Developer"
+      content="Andrew Moses - Software Engineer | Web Developer | JavaScript Tutor"
     />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://www.andrewmoses.dev" />
     <meta property="og:image" content="/icon.png" />
     <meta
       property="og:description"
-      content="Experienced software engineer and web developer focused on creating fast, scalable, and accessible applications with strong SEO."
+      content="Experienced software engineer, web developer, and JavaScript tutor focused on creating fast, scalable, and accessible applications with strong SEO."
     />
   </Head>
 );
