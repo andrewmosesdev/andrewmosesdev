5da3bf11c99d3a7ce063fe13842b6b33e1d1db39
Andrew Moses <andrewmosesdrive@gmail.com>
2022-12-29 16:52:22 -0700

    update about

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ const About = (): JSX.Element => (
     <h2 className="mb-10 px-[8px] whitespace-normal text-center">
       Experienced software engineer and web developer located
       in Phoenix, Arizona. I focus on creating performant, scalable, and
-      accessible applications, and helping others grow their business.
+      accessible applications.
     </h2>
   </section>
 );
