5190a51be0091b9d8271f7c13553bd56b5cd33f5
Andrew Moses <andrewmosesdrive@gmail.com>
2022-12-29 18:56:28 -0700

    test css effect

--- components/TitleSection.tsx
+++ components/TitleSection.tsx
@@ -1,7 +1,7 @@
 const TitleSection = (): JSX.Element => (
   <section className="text-center">
     <h1 className="text-6xl">
-      <span className="text-5xl block mb-10">Andrew Moses</span>{" "}
+      <span className="text-5xl block mb-10 title-effect">Andrew Moses</span>{" "}
       <span className="text-3xl mb-10 flex flex-col">
         <span className="mb-[8px]">Software Engineer </span>{" "}
         <span className="mb-[8px]">Web Developer </span>{" "}
