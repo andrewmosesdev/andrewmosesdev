68d92eafc25be1de36cc7ed310793afd7af3f0ef
Andrew Moses <andrewmosesdrive@gmail.com>
2023-01-01 17:16:34 -0700

    no y axis movement about me

--- components/About.tsx
+++ components/About.tsx
@@ -5,14 +5,12 @@ const About = (): JSX.Element => (
   <section className="w-full sm:max-w-[560px] mb-10 px-[8px] whitespace-normal text-center">
     <motion.h3
       variants={{
-        [VARIANT.HIDE]: { opacity: 0, y: 25 },
+        [VARIANT.HIDE]: { opacity: 0 },
         [VARIANT.SHOW]: {
           opacity: 1,
           transition: {
             delay: 1.2,
-            y: { bounce: 0.5, duration: 1, type: "spring" },
           },
-          y: 0,
         },
       }}
     >
